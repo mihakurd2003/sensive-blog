7d503829640878c19f5cb1f7b3d1b760e2960d0a
S1im_Sh4dy <mihakurd@gmail.com>
2023-04-11 17:26:18 +0300

    - Deleted media/ - Used get_object_or_404 - Reduced number of queries to the DB - Changed requirements.txt

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post, Tag
 from django.db.models import Count
 
@@ -46,8 +46,8 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.annotate(likes_count=Count('likes')).get_prefetch_data_for_posts().get(slug=slug)
-    comments = post.comments.prefetch_related('author')
+    post = get_object_or_404(Post.objects.annotate(likes_count=Count('likes')).get_prefetch_data_for_posts(), slug=slug)
+    comments = post.comments.select_related('author')
     serialized_comments = []
     for comment in comments:
         serialized_comments.append({
@@ -85,7 +85,7 @@ def post_detail(request, slug):
 
 
 def tag_filter(request, tag_title):
-    tag = Tag.objects.get(title=tag_title)
+    tag = get_object_or_404(Tag, title=tag_title)
 
     most_popular_tags = Tag.objects.popular()[:5]
 
